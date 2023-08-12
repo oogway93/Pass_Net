ffb38afe6741a933a25dfbf27d2125cc4413d088
oogway93 <lemeshinskiym@gmail.com>
2023-08-13 00:03:41 +0300

    first version of handler password

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from src.routers.routers import router
 
 app = FastAPI()
 
+app.include_router(router)
+
 if __name__ == '__main__':
-    uvicorn.run()
+    uvicorn.run('main:app')
--- src/routers/routers.py
+++ src/routers/routers.py
@@ -1,15 +1,36 @@
 from fastapi import APIRouter
-from typing import Optional
+from typing import Optional, Annotated
 import random
 
-router = APIRouter(prefix='/pass')
+router = APIRouter(prefix='/pass', tags=['PASSWORD'])
 
 
 @router.get('/password')
-async def password(length: int, uppercase_chars: Optional[bool], digits: Optional[bool], special_chars: Optional[bool]):
-    generated_password = ''
+async def password(length: int = 7, uppercase_chars: Optional[bool] = False, digits: Optional[bool] = None,
+                   special_chars: Optional[bool] = None, hard_mode: Optional[bool] = None):
+    generated_password_1 = ''
+    generated_password_2 = ''
+    generated_password_3 = ''
+    generated_password_4 = ''
+    generated_password_5 = ''
     initial = list('abcdefghijklmnopqrstuvwxyz')
     if uppercase_chars:
         initial.extend(list('ABCDEFGHIJKLMNOPQRSUVWXYZ'))
     if digits:
         initial.extend(list('1234567890'))
+    if special_chars:
+        initial.extend((list('!@#$%^&*')))
+    if hard_mode:
+        initial.extend(list('~()_-+={[}]|\:;><.?/'))
+    for i in range(length):
+        generated_password_1 += random.choice(initial)
+        generated_password_2 += random.choice(initial)
+        generated_password_3 += random.choice(initial)
+        generated_password_4 += random.choice(initial)
+        generated_password_5 += random.choice(initial)
+
+    return {'Your pass_1 is ready': generated_password_1,
+            'Your pass_2 is ready': generated_password_2,
+            'Your pass_3 is ready': generated_password_3,
+            'Your pass_4 is ready': generated_password_4,
+            'Your pass_5 is ready': generated_password_5}

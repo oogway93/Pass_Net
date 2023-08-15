a4556df38ae34812aa2a59e988f03302cab6aa15
oogway93 <lemeshinskiym@gmail.com>
2023-08-15 23:32:42 +0300

    modification in router login

--- src/login/routers.py
+++ src/login/routers.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import APIRouter, HTTPException
 import random
 import string
@@ -6,11 +8,27 @@ router = APIRouter(prefix="/login")
 
 
 @router.get("/random_login")
-async def generate_random_login(length: int = 8):
+async def generate_random_login(length: int = 8, fluent_log: Optional[bool] = None, nickname: Optional[str] = None):
     if 8 <= length <= 20:
-        characters = string.ascii_letters + string.digits
-        login = ''.join(random.choice(characters) for _ in range(length))
-        return login
+        additions = ['cool', 'awesome', 'master', 'ninja', 'pro', 'gamer', 'hacker', 'star', 'legend', 'expert',
+                     'junior', 'profi', 'senior', 'middle', 'sniper', 'pudge', 'lol', 'wow', 'lil']
+
+        if fluent_log and nickname:
+            fluent_characters = string.digits
+            fluent_login = ''.join(random.choice(fluent_characters) for _ in range(length - len(nickname)))
+            return {"fluent login": f"{nickname}{random.choice(additions).capitalize()}{fluent_login}"}
+        elif fluent_log:
+            raise HTTPException(status_code=500, detail={"status": "error", "data": None,
+                                                         "detail": "If you wanna fluent login, you must enter your "
+                                                                   "attractive nickname."})
+        elif nickname:
+            raise HTTPException(status_code=500, detail={"status": "error", "data": None,
+                                                         "detail": "If you wanna enter nickname, you must confirm ("
+                                                                   "write true) 'fluent log'."})
+        else:
+            characters = string.ascii_letters + string.digits
+            login = ''.join(random.choice(characters) for _ in range(length))
+            return {"simple login": login}
     else:
         raise HTTPException(status_code=500, detail={"status": "error", "data": None,
                                                      "detail": "Length must be between 8 and 20."})
